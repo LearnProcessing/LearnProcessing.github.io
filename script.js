5c0a181d3b3a4c30108e17948f97aeac45e0a3e1
LearnProcessing <rinaldiadrian5@gmail.com>
2020-12-17 15:40:12 +0700

    kasi judul

---
+++ script.js
@@ -0,0 +1,7 @@
+let judulGame = "Mbah Dukun sedang mengobati pasiennya. Bantu dia meracik ramuan!"
+var i = 0;
+var txt = '';
+var speed = 50;
+
+document.getElementById("judul-game").innerHTML = judulGame
+
